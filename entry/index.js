e27cf82d1f5c3805a69006d01fa4e59d2c5b016d
traviswimer <traviswimer@users.noreply.github.com>
2022-08-21 12:38:55 -0400

    Changes ESM entry file to export methods individually.
    This prevents a warning from being displayed when importing from this package.

--- entry/index.js
+++ entry/index.js
@@ -1,2 +1,19 @@
-import * as ReactHydrationProvider from "../dist/index.js";
-export default ReactHydrationProvider;
+import {
+	HydrationContext,
+	HydrationProvider,
+	useHydrated,
+	Server,
+	Client,
+	createHydration,
+	useComponentHydrated,
+} from "../dist/index.js";
+
+export {
+	HydrationContext,
+	HydrationProvider,
+	useHydrated,
+	Server,
+	Client,
+	createHydration,
+	useComponentHydrated,
+};
